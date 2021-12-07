c82f041072d1ee6c208027dd102fae07bde75f76
vetsinen <vetsinen@gmail.com>
2021-12-07 15:48:38 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const path = require("path");
+const puppeteer = require("puppeteer");
+const mustache = require("mustache");
+const fileType = require("file-type");
+
+(async () => {
+  const template = await fs.promises.readFile(
+    "./assets/postalLayout.sv-1.html",
+    {
+      encoding: "utf8",
+    }
+  );
+
+  const partials = {
+    content: await fs.promises.readFile(
+      "./assets/postalPersonalizedBookingConfirmedBody.sv-1-fixed.html",
+      { encoding: "utf8" }
+    ),
+  };
+
+  const view = {
+    appUrl: "https://aleris.strikersoft.dev",
+    marketingUrl: "https://aleris.se",
+    tenantName: "Aleris",
+    careunitName: "Aleris Vaccination & Resemedicin",
+    careunitAddress: "Olof Palmes Gata 9, tr 3, Stockholm",
+    careunitPhoneNumber: "+380441111111",
+    patientPersonalNumber: "191212121212",
+    patientFullName: "Tolvan Tolvansson",
+    nowDate: "2021-10-20",
+    patientAddress: "line1, line2",
+    patientZipCode: "113 51",
+    patientPostOffice: "Stockholm",
+    visitDate: "2021-10-21",
+    visitStartTime: "12:00",
+    visitEndTime: "12:30",
+  };
+
+  let html = mustache.render(template, view, partials);
+
+  for (const [cid, fileName] of html.matchAll(/"cid:([^"]*)"/g)) {
+    const filePath = path.join("./assets", fileName);
+    const base64 = await fs.promises.readFile(filePath, {
+      encoding: "base64",
+    });
+
+    const { mime } = await fileType.fromFile(filePath);
+    html = html.replace(cid, `"data:${mime};base64, ${base64}"`);
+  }
+
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+  });
+
+  const fileName = new Date().toISOString();
+  await fs.promises.writeFile(`${fileName}.html`, html);
+  const page = await browser.newPage();
+  await page.setContent(html);
+  const buffer = await page.pdf({
+    preferCSSPageSize: true,
+  });
+
+  await page.close();
+  await browser.close();
+  await fs.promises.writeFile(`${fileName}.pdf`, buffer);
+})();
